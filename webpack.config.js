925e68e9291b0feda66115bce17dd6082af84694
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-10-10 17:01:33 +0100

    Remove react-refetch dependencies and fill in boilerplate from atlas-package

--- webpack.config.js
+++ webpack.config.js
@@ -1,133 +1,97 @@
-var webpack = require('webpack');
-var path = require('path');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require(`webpack`);
+const path = require(`path`);
+const CleanWebpackPlugin = require(`clean-webpack-plugin`);
 
-process.traceDeprecation = true;
+const config = {
+  entry: {
+    atlasAutocomplete: [`babel-polyfill`, `./html/render.js`],
+    dependencies: [`prop-types`, `react`, `react-dom`, `react-autocomplete`, `urijs`]
+  },
 
-module.exports = {
-    entry: {
-        atlasAutocomplete: ['whatwg-fetch', './html/render.js'],
-        dependencies: ['prop-types', 'react', 'react-dom', 'react-autocomplete', 'urijs']
-    },
+  output: {
+    library: `[name]`,
+    path: path.resolve(__dirname, `dist`),
+    filename: `[name].bundle.js`,
+    publicPath: `/dist/`
+  },
 
-    output: {
-        library: '[name]',
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-        publicPath: '/dist/'
-    },
+  plugins: [
+    new CleanWebpackPlugin([`dist`], {verbose: true, dry: false}),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: `dependencies`,
+      filename: `vendorCommons.bundle.js`,
+      minChunks: Infinity     // Explicit definition-based split, see dependencies entry
+    })
+  ],
 
-    plugins: [
-        new CleanWebpackPlugin(['dist'], {verbose: true, dry: false}),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'dependencies',
-            filename: 'vendorCommons.bundle.js',
-            minChunks: Infinity     // Explicit definition-based split, see dependencies entry
-        }),
-        // new webpack.HotModuleReplacementPlugin(),
-        // enable HMR globally, necessary along with devServer.hot: true (see below) for HMR to work as expected 🤔
-        // new webpack.NamedModulesPlugin()
-        // prints more readable module names in the browser console on HMR updates
-    ],
-
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules:false
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules:false
-                        }
-                    },
-                    {
-                        loader: 'less-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.(jpe?g|png|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            query: {
-                                name: '[hash].[ext]',
-                                hash: 'sha512',
-                                digest: 'hex'
-                            }
-                        }
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            query: {
-                                bypassOnDebug: true,
-                                mozjpeg: {
-                                    progressive: true,
-                                },
-                                gifsicle: {
-                                    interlaced: true,
-                                },
-                                optipng: {
-                                    optimizationLevel: 7,
-                                }
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(svg)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            query: {
-                                name: '[hash].[ext]',
-                                hash: 'sha512',
-                                digest: 'hex'
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.jsx?$/,
-                // Place after node_modules packages owned by Expression Atlas to be transpiled, as they aren’t
-                // distributed pre-bundled or with a dist kind of folder
-                exclude: /node_modules\/(?!(expression-atlas|anatomogram|react-ebi-species))/,
-                use: [
-                    {
-                        loader: 'babel-loader'
-                    }
-                ]
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [ `style-loader`, `css-loader` ]
+      },
+      {
+        test: /\.less$/i,
+        use: [ `style-loader`, `css-loader`, `less-loader` ]
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        use: [
+          {
+            loader: `file-loader`,
+            options: {
+              query: {
+                name: `[hash].[ext]`,
+                hash: `sha512`,
+                digest: `hex`
+              }
             }
+          },
+          {
+            loader: `image-webpack-loader`,
+            options: {
+              query: {
+                bypassOnDebug: true,
+                mozjpeg: {
+                  progressive: true,
+                },
+                gifsicle: {
+                  interlaced: true,
+                },
+                optipng: {
+                  optimizationLevel: 7,
+                }
+              }
+            }
+          }
         ]
-    },
+      },
+      {
+        test: /\.(svg)$/i,
+        use: [
+          {
+            loader: `file-loader`,
+            options: {
+              query: {
+                name: `[hash].[ext]`,
+                hash: `sha512`,
+                digest: `hex`
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules\//,
+        use: `babel-loader`
+      }
+    ]
+  },
 
-    devServer: {
-        // hot: true,      // CLI --hot is equivalent to this option, but it also enables the HMR plugin (see above)
-        // hotOnly: true,  // Won’t inject modules if there’s a compilation error (without this a full page reload is
-                        // done after a successful build and we lose state)
-        port: 9000
-    }
-};
+  devServer: {
+    port: 9000
+  }
+}
+
+module.exports = config
